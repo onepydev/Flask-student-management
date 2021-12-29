2ce9d99850bbe239d685508715ae764876fde7ae
netbitsolutions <henryofozor@gmail.com>
2021-12-29 08:26:29 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,7 @@
-from myapp import app,db
+from myapp import app
 
 if __name__ =="__main__":
-    #db.create_all()
-    app.run(debug=True,port=5112,host= '127.0.0.1',threaded=True)
-    #manager.run()
-    ## na to manage am  henry
-#$ python manage.py db init 
-#$ python manage.py db migrate
-#$ python manage.py db upgrade
-#$ python manage.py db --help
-# To migrate the Db, first disable the app.run then enable the manager.run then run the python commands on poweer shell
+    app.run()
+    #app.run(debug=True,port=5112,host= '127.0.0.1',threaded=True)
 
 
